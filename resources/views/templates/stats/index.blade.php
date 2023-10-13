c962bc781079d798e3e94775442949e821cfdf97
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-13 15:06:40 +0300

    init commit

--- resources/views/templates/stats/index.blade.php
+++ resources/views/templates/stats/index.blade.php
@@ -1,4 +1,4 @@
-<div class="py-4 md:py-11 bg-gray-50">
+<div class="py-4 md:py-4 bg-white">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-7xl sm:text-center">
 
