57bbb1e7164e21d5b33766963af0901859c878cd
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-13 14:30:08 +0300

    init commit

--- resources/views/templates/full_image_width/index.blade.php
+++ resources/views/templates/full_image_width/index.blade.php
@@ -1,7 +1,7 @@
-<section class="">
+<section class="-mb-24">
     <img
         loading="lazy"
-        class="w-full object-cover py-24 -pb-12"
+        class="w-full object-cover py-24 -pb-24"
         src="{{  \Illuminate\Support\Facades\Storage::url($section->extra['image']) }}"
         alt="{{ settings('site_name') }}"
     >
