632dd917f490c30af6694581c25e4861d6a05c04
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-13 15:05:58 +0300

    init commit

--- resources/views/templates/stats/index.blade.php
+++ resources/views/templates/stats/index.blade.php
@@ -8,8 +8,8 @@
                      loading="lazy"
                      alt="{{ $section->extra['heading'] }}">
 
-                <div class="mx-auto grid max-w-7xl">
-                    <div class="px-6 pb-24 pt-16 sm:pb-32 sm:pt-20 lg:col-start-2 lg:px-8 lg:pt-32">
+                <div class="">
+                    <div class="">
                         <div class="mx-auto max-w-2xl lg:mr-0 lg:max-w-lg">
                             <h2 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ $section->extra['heading'] }}</h2>
                             <p class="mt-6 text-lg leading-8 text-gray-600">{{ $section->extra['heading'] }}.</p>
