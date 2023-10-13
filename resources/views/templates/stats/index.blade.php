35fa0440ebbe616c0daa0752668ffa4ce2a3531d
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-13 14:38:25 +0300

    init commit

--- resources/views/templates/stats/index.blade.php
+++ resources/views/templates/stats/index.blade.php
@@ -1,18 +1,47 @@
 <div class="relative bg-white">
-    <img class="h-56 w-full bg-gray-50 object-cover lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-1/2" src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2850&q=80" alt="">
-    <div class="mx-auto grid max-w-7xl lg:grid-cols-2">
+    <img class="h-56 w-full bg-gray-50 object-cover lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-1/2"
+         src="{{ url(Storage::url($section->extra['bg_image'])) }}"
+         loading="lazy"
+         alt="{{ $section->extra['heading'] }}">
+    <div {{ $animationEffect }}  class="mx-auto grid max-w-7xl lg:grid-cols-2">
         <div class="px-6 pb-24 pt-16 sm:pb-32 sm:pt-20 lg:col-start-2 lg:px-8 lg:pt-32">
             <div class="mx-auto max-w-2xl lg:mr-0 lg:max-w-lg">
-                <h2 class="text-base font-semibold leading-8 text-indigo-600">Our track record</h2>
-                <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Trusted by thousands of creators&nbsp;worldwide</p>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                <h2 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ $section->extra['heading'] }}</h2>
+                <p class="mt-6 text-lg leading-8 text-gray-600">{{ $section->extra['heading'] }}.</p>
+
                 <dl class="mt-16 grid max-w-xl grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 xl:mt-16">
-                    <div class="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
-                        <dt class="text-sm leading-6 text-gray-600">Creators on the platform</dt>
-                        <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">8,000+</dd>
+                    @foreach($section->extra['counts'] as $stat)
+                    <div class="flex flex-col gap-y-3 border-l border-secondary-900/10 pl-6">
+                        <dt class="text-sm leading-6 text-gray-600">{{ $stat['title'] }}</dt>
+                        <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">
+                            <div class="flex justify-center items-center text-center">
+                                <dd
+                                    class="order-first text-3xl font-semibold tracking-tight text-gray-900">
+                                    <div
+                                        x-data="animatedCounter( {{ $stat['count'] }}, 200, 0)"
+                                        x-text="Math.round(current.toFixed(2))"
+                                        x-intersect:enter="updatecounter"
+                                        x-intersect:leave="current=0"
+                                        x-transition
+                                    >
+
+
+                                    </div>
+
+                                </dd>
+                                @if($stat['has_plus_icon'])
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+
+                                @endif
+                            </div>
                     </div>
-                    <div class="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
-                        <dt class="text-sm leading-6 text-gray-600">Flat platform fee</dt>
+                        </dd>
+                    </div>
+                    @endforeach
+                    <div class="flex flex-col gap-y-3 border-l border-secondary-900/10 pl-6">
+                        <dt class="text-sm leading-6 text-gray-600"></dt>
                         <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">3%</dd>
                     </div>
                     <div class="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
@@ -24,6 +53,7 @@
                         <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">$70M</dd>
                     </div>
                 </dl>
+
             </div>
         </div>
     </div>
