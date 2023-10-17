31af8f616772fcc667c665305f469cbbb69100e5
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-17 19:43:08 +0300

    init commit

--- plugins/teams/src/Models/TeamCategory.php
+++ plugins/teams/src/Models/TeamCategory.php
@@ -9,4 +9,14 @@ class TeamCategory extends Model
     protected $casts = [
         'extra' => 'json'
     ];
+
+    public function companyTeams()
+    {
+        return $this->belongsToMany(
+            related: CompanyTeam::class,
+            table: 'company_team_categories',
+            foreignPivotKey: 'company_team_id',
+            relatedPivotKey: 'team_category_id'
+        );
+    }
 }
