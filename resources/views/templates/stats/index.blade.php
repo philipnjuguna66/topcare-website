50746e85b6eb3d13da8b9db184c8491ccb46a626
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-17 17:05:47 +0300

    init commit

--- resources/views/templates/stats/index.blade.php
+++ resources/views/templates/stats/index.blade.php
@@ -8,22 +8,30 @@
             <dl class="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
                 @foreach($section->extra['counts'] as $stat)
                 <div class="flex flex-col bg-gray-400/5 p-8">
-                    <x-dynamic-component :component=" $stat['icon'] " class="mt-4 h-8 w-8" />
+                    <x-dynamic-component :component=" $stat['icon'] " class="mt-4 h-12 w-12 text-center" />
                     <dt class="text-sm font-semibold leading-6 text-gray-600">{{  $stat['title'] }}</dt>
-                    <dd class="order-first text-3xl font-semibold tracking-tight text-gray-50 text-center"
-                        x-data="animatedCounter( {{ $stat['count'] }}, 200, 0)"
-                        x-text="Math.round(current.toFixed(2))"
-                        x-intersect:enter="updatecounter"
-                        x-intersect:leave="current=0"
-                        x-transition>
-                    </dd>
+                    <div class="flex  flex-row text-center">
+                        <dd class="order-first text-3xl font-semibold tracking-tight text-gray-50 text-center"
+                            x-data="animatedCounter( {{ $stat['count'] }}, 200, 0)"
+                            x-text="Math.round(current.toFixed(2))"
+                            x-intersect:enter="updatecounter"
+                            x-intersect:leave="current=0"
+                            x-transition>
+                        </dd>
+
+
+                        @if($stat['has_plus_icon'])
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-center text-secondary-500">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                        @endif
+                    </div>
                 </div>
                 @endforeach
             </dl>
         </div>
     </div>
-
-
+</div>
 @push('scripts')
 
 
@@ -50,4 +58,3 @@
         }
     </script>
 @endpush
-</div>
