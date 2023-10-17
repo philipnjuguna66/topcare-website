fa537b92b8668fd51e69e52367b6f4852692bbf6
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-17 11:56:43 +0300

    init commit

--- resources/views/pages/property/single.blade.php
+++ resources/views/pages/property/single.blade.php
@@ -10,7 +10,7 @@
     <div class="bg-gray-50">
 
 
-        <div class="mx-auto md:w-4/5 max-w-7xl py-12 md:mt-20 md:py-16 px-8">
+        <div class="mx-auto md:w-4/5 max-w-7xl py-8 md:mt-20 md:py-8 px-8">
             <h1 class="py-12 md:py-4 font-extrabold text-2xl lg:text-4xl text-center uppercase px-8 md:px-0">{{ $page->name }}</h1>
 
             <div class="grid grid-cols-1 md:grid-cols-6 gap-4 py-8 max-w-7xl">
