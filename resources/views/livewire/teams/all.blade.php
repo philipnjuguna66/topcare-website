a3eec5027e2fd180decb5d4d29486cdca56f7bc2
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-17 19:52:49 +0300

    init commit

--- resources/views/livewire/teams/all.blade.php
+++ resources/views/livewire/teams/all.blade.php
@@ -14,6 +14,7 @@
         @endforeach
 
     </ul>
+    @dump($tab)
 
 
 
@@ -21,7 +22,7 @@
 
         @foreach($teams as $item)
 
-            @dump($item)
+
 
             @include('templates.teams.director',['team' => $item])
 
