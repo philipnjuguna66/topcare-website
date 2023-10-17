5b0c3392e2361969913c0f4de76b9e0bb8274859
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-17 16:32:28 +0300

    init commit

--- resources/views/pages/single.blade.php
+++ resources/views/pages/single.blade.php
@@ -6,7 +6,7 @@
         @meta("description", $page->meta_description)
     @endpush
 
-    <div class="mt-4 md:mt-8 py-12 ">
+    <div class="mt-8 md:mt-8 py-12 ">
         @foreach($page->sections as $section)
 
             @php
