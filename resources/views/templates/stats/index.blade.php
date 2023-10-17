da3f32c611910a4760458e01783062037b3d7c73
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-17 16:56:28 +0300

    init commit

--- resources/views/templates/stats/index.blade.php
+++ resources/views/templates/stats/index.blade.php
@@ -1,5 +1,32 @@
-<div class="py-4 md:py-4 bg-primary-600">
+<div class="bg-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:max-w-none">
+            <div class="text-center">
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Trusted by creators worldwide</h2>
+                <p class="mt-4 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet consect adipisicing possimus.</p>
+            </div>
+            <dl class="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
+                @foreach($section->extra['counts'] as $stat)
+                <div class="flex flex-col bg-gray-400/5 p-8">
+                    <x-dynamic-component :component="{{ $stat['icon'] }}" class="mt-4" />
+                    <dt class="text-sm font-semibold leading-6 text-gray-600">{{  $stat['title'] }}</dt>
+                    <dd class="order-first text-3xl font-semibold tracking-tight text-gray-50 text-center"
+                        x-data="animatedCounter( {{ $stat['count'] }}, 200, 0)"
+                        x-text="Math.round(current.toFixed(2))"
+                        x-intersect:enter="updatecounter"
+                        x-intersect:leave="current=0"
+                        x-transition>
+                    </dd>
+                </div>
+                @endforeach
+            </dl>
+        </div>
+    </div>
+</div>
+
+
+<div class="py-4 md:py-4 bg-primary-600">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 hidden">
         <div class="mx-auto max-w-7xl sm:text-center">
 
 
