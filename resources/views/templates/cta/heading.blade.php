92c8f531af26fab69ff6877226f808f250fd79e8
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-26 14:28:08 +0300

    init commit

--- resources/views/templates/cta/heading.blade.php
+++ resources/views/templates/cta/heading.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-white -mt-2 px-6 py-8 md:py-12 lg:px-8">
+<div class="bg-white px-6 py-12 md:py-24 lg:px-8">
     <div class="mx-auto max-w-5xl text-center">
-        <h2 class="text-4xl font-bold tracking-tight sm:text-6xl">{{ $section->extra['heading'] }}</h2>
+        <h1 class="text-4xl font-bold tracking-tight sm:text-6xl">{{ $section->extra['heading'] }}</h1>
         <p class="mt-6 text-lg leading-8 text-gray-600 prose">
             {!!  $section->extra['content'] !!}
         </p>
