a00f6975f7de002dd10f12f78b8c7bf11dbdfe3d
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-27 15:24:28 +0300

    hero with sections make it dynamic

--- resources/views/pages/single.blade.php
+++ resources/views/pages/single.blade.php
@@ -6,7 +6,7 @@
         @meta("description", $page->meta_description)
     @endpush
 
-    <div class="mt-8 md:mt-8 md:pt-24">
+    <div class="mt-8 md:mt-8 md:pt-32">
         @foreach($page->sections as $section)
 
             @php
