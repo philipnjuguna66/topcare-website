23000837e5e18ec1ca9aa407918fd163c55d3906
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-27 14:33:31 +0300

    hero with sections make it dynamic

--- resources/views/templates/hero/_text_area.blade.php
+++ resources/views/templates/hero/_text_area.blade.php
@@ -1,8 +1,6 @@
+<div class="px-2 mt-5 ">
 
-
-<div class="font-extrabold bg-white shadow-2xl rounded-xl px-2 mt-5 bg-gray-100 border-b-4 border-primary-600 border-b-primary-600">
-
-    <div class="prose">
+    <div class="prose py-4">
         {{ str($html)->toHtmlString() }}
     </div>
 
