85c66de7fb6de327c9710c13323d7811dc2e838b
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-27 15:40:59 +0300

    hero with sections make it dynamic

--- resources/views/templates/stats/index.blade.php
+++ resources/views/templates/stats/index.blade.php
@@ -9,13 +9,13 @@ background-image: url({{ \Illuminate\Support\Facades\Storage::url($section->extr
 
 
     </div>
-    <div class="mx-auto max-w-7xl px-6 lg:px-8 z-2 absolute md:left-[50%] md:top-[50%] fixed" style="transform: translate(-50%, -50%); ">
-        <div class="mx-auto max-w-7xl lg:max-w-none">
+    <div class="mx-auto max-w-8xl px-6 lg:px-4 z-2 absolute md:left-[50%] md:top-[50%] fixed" style="transform: translate(-50%, -50%); ">
+        <div class="">
             <div class="text-center text-white">
                 <h2 class="text-3xl font-bold tracking-tight sm:text-4xl">{{ $section->extra['heading'] }}</h2>
                 <p class="mt-4 text-lg leading-8 ">{{ $section->extra['subheading'] }}</p>
             </div>
-            <dl class="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4 z-2 opacity-100 "  {{ $animationEffect }}>
+            <dl class="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4 z-20 opacity-100 "  {{ $animationEffect }}>
                 @foreach($section->extra['counts'] as $stat)
                     <div class="flex flex-col p-8 text-center bg-green-400 text-gray-100 opacity-100">
                         <x-dynamic-component :component=" $stat['icon'] " class="mt-4 mx-auto h-12 w-12 text-center" />
