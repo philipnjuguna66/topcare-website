f0103d3b853b3e90f35d833a3691fce5dd27bf88
Njuguna Philip <philipnjuguna66@gmail.com>
2023-10-27 14:02:08 +0300

    hero with sections make it dynamic

--- resources/views/templates/hero/service_hero.blade.php
+++ resources/views/templates/hero/service_hero.blade.php
@@ -1,16 +1,11 @@
 <div class="md:pt-24 bg-gray-50 @if($section->extra['bg_white'] )  bg-white @endif">
     <div class="mx-auto w-4/5 max-w-7xl px-2 lg:px-8" {{ $animationEffect }}>
-
-        <div class="grid grid-cols-1 md:grid-cols-{{ $section->extra['columns'] }}  gap-12 space-y-4 mt-4 py-4">
-
+        <div class="grid grid-cols-1 md:grid-cols-{{ $section->extra['columns'] }}  gap-3 space-y-4 mt-4 py-4">
             @foreach($section->extra['columns_sections'] as $index => $columns)
 
                <div class="text-justify">
 
-
                    @foreach($columns as $column)
-
-
 
                        <?php
                            $html = match ($column['type'])
