3e06a003596c90a02c3b25abea0b0154627bcbe6
Njuguna Philip <philipnjuguna66@gmail.com>
2023-11-02 18:56:28 +0300

    wip

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -71,8 +71,8 @@
     </div>
 
     <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="/" class="flex items-center">
+        <div class="max-w-screen-xl flex flex-row flex-wrap items-center justify-between mx-auto p-4">
+            <a href="/" class="flex items-center hidden md:block">
                 <img src="{{ \Illuminate\Support\Facades\Storage::url($options?->logo ) }}" class="h-8 mr-3" alt="{{ $options?->name }}" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ $options?->name }}</span>
             </a>
