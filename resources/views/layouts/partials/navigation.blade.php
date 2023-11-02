bb67e0c7d07f10477adedcc45c250823bd647f61
Njuguna Philip <philipnjuguna66@gmail.com>
2023-11-02 18:28:09 +0300

    navigation links

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -72,7 +72,7 @@
 
 
     <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 shadow-md shadow-primary-950/50">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4 px-2">
+        <div class="max-w-7xl flex flex-wrap items-center justify-between mx-auto py-4 px-2">
             <a href="/" class="flex items-center">
                 <img src="{{ \Illuminate\Support\Facades\Storage::url($options?->logo ) }}" class="h-8 md:h-16 mr-3" alt="{{ $options?->name }}" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ $options?->name }}</span>
