b0e20ac6801ee890449cc564476208ee44711656
Njuguna Philip <philipnjuguna66@gmail.com>
2023-11-02 18:52:13 +0300

    wip

--- resources/views/templates/projects/index.blade.php
+++ resources/views/templates/projects/index.blade.php
@@ -1,5 +1,5 @@
 <div class="  @if($section->extra['bg_white']  ) bg-white @endif py-24 mt-4">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl px-2 lg:px-4">
         <div class="mx-auto max-w-5xl text-center">
             <h1 class="text-2xl  md:text-5xl font-bold tracking-tight"> {{ str($section->extra['heading'])->toHtmlString() }}</h1>
             <p class="py-3 text-lg leading-8 text-gray-600">{{ str($section->extra['subheading'])->toHtmlString() }}.</p>
