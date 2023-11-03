965b691adcd7cf1fff89c60d9c0c987fc314fda2
Njuguna Philip <philipnjuguna66@gmail.com>
2023-11-03 12:25:26 +0300

    wip

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -69,14 +69,14 @@
 
 
     <nav class="sticky  top-0 left-0 right-0 z-50  bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 shadow-md shadow-primary-950/50">
-        <div class="flex flex-row justify-between mx-auto max-w-7xl w-4/5  sm:py-20">
+        <div class="flex flex-row justify-between mx-auto max-w-7xl w-4/5 ">
             <a href="/" class="flex items-center">
                 <img src="{{ \Illuminate\Support\Facades\Storage::url($options?->logo ) }}" class="h-8 md:h-16 mr-3" alt="{{ $options?->name }}" />
 
             </a>
             <button data-collapse-toggle="navbar-multi-level" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <svg class="w-5 h-5 font-semibold text-2xl" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                 </svg>
             </button>
