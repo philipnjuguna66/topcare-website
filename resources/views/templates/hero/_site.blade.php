5384db54decbc0404a562ce9c03e4c086c320e4c
Njuguna Philip <philipnjuguna66@gmail.com>
2023-11-06 13:26:41 +0300

    wip

--- resources/views/templates/hero/_site.blade.php
+++ resources/views/templates/hero/_site.blade.php
@@ -1,5 +1,5 @@
 <div class="py-2 dark:text-white">
-    <div class="relative  px-6 pb-12 pt-12 sm:pt-12 lg:static lg:px-8 lg:py-12">
+    <div class="relative  md:px-6 md:pb-12 md:pt-12 sm:pt-12 lg:static lg:px-8 lg:py-12">
         <livewire:contact.book-site-visit/>
     </div>
 </div>
