76781e9e9a9ff39f4baa3956cbc00c1d254f747d
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-09 16:16:49 +0300

    wip

--- app/Http/Livewire/Contact/BookSiteVisit.php
+++ app/Http/Livewire/Contact/BookSiteVisit.php
@@ -108,6 +108,11 @@ class BookSiteVisit extends Component implements HasForms
                 'page' => isset($this->page->title) ? $this->page->title : $branch,
             ]);
 
+            Http::post('https://shekinahhub.com//api/v1/crm/create', [
+                'phone_number' => $data['phone_number'],
+                'source' => "website",
+            ]);
+
             event(new LeadCreatedEvent(
                 lead: $lead,
                 branch: $branch,
