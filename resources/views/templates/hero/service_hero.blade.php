a853eed1e7befc57151d82e16cecf7064ed084ba
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-23 19:10:05 +0300

    wip

--- resources/views/templates/hero/service_hero.blade.php
+++ resources/views/templates/hero/service_hero.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-gray-50 md:py-12 md:mt-3 @if($section->extra['bg_white'] )  bg-white @endif">
-    <div class="md:mx-auto md:w-4/5 max-w-7xl  lg:px-8">
+    <div class="md:mx-auto md:w-4/5 max-w-7xl px-2 lg:px-8">
         <div class="  grid grid-cols-1 md:grid-cols-{{ $section->extra['columns'] }}  gap-x-4 gap-y-2 mt-4 py-4">
             @foreach($section->extra['columns_sections'] as $index => $columns)
                <div class="md:text-justify max-w-7xl">
